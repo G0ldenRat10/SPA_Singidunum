d9570239980c3a5fe9da22c96e5ccbfafa195f80
Lazar Tatomir <lazar.tatomir.25@singimail.rs>
2026-02-20 23:24:00 +0100

    Added Day_1 Selection sort

---
+++ selection_sort.py
@@ -0,0 +1,14 @@
+arr = [5,3,1,4,2]
+
+def select_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1,n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+print(arr)
+print(select_sort(arr))
